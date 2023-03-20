f6e63f12e6a877b3cdbcc95f4429e15e18ce474e
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2023-03-20 04:15:17 +0100

    Update [slug].js

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react'
 import { client, urlFor } from "../../lib/client";
+import { Product } from "../../components";
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from "react-icons/ai";
 
 const ProductDetails = ({ product, products }) => {
@@ -76,6 +77,16 @@ const ProductDetails = ({ product, products }) => {
                     onClick="">
                         Buy Now
                     </button>
+                </div>
+            </div>
+        </div>
+        <div className='maylike-products-wrapper'>
+            <h2>You may also like</h2>
+            <div className='marquee'>
+                <div className="maylike-products-container track">
+                    {products.map((item) => (
+                        <Product key={item._id} product={item}/>
+                    ))}
                 </div>
             </div>
         </div>
