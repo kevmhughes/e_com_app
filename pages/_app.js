934046f9aa1f72e228855aed1a9fa4b1beec5a36
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2023-03-21 01:40:15 +0100

    Update _app.js and create StateContext.js with all the apps states

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,20 @@
-import '@/styles/globals.css'
+import React from "react";
+import { Toaster } from "react-hot-toast"
+
+import '../styles/globals.css'
 import { Layout } from  "../components"
+import { StateContext } from '../context/StateContext'
+
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+    
     
   )
 
