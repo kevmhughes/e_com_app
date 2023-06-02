91a8873a90f16e6fc9f0795ee15c6e6b2b18e951
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2023-06-03 01:38:44 +0200

    Update success.js and create utils.js
    Add confetti animation to success page on successfully completing transaction

--- pages/success.js
+++ pages/success.js
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/link-passhref */
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, {useState, useEffect } from 'react';
 import Link from "next/link";
 import { BsBagCheckFill } from "react-icons/bs";
