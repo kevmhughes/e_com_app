fa5168bbe89b0f0a7c9780229a86f3844df2f577
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2023-09-08 02:12:52 +0200

    Update [slug].js

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -63,7 +63,7 @@ const ProductDetails = ({ product, products }) => {
             </p>
           </div>
           <div className="buttons">
-            <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Add to Cart</button>
+            <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Add To Cart</button>
             <button type="button" className="buy-now" onClick={handleBuyNow}>Buy Now</button>
           </div>
         </div>
